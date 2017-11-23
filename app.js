8ca4bf0c5eb93b943a5ccefcf51015c761f8cee3
Santi Valdez <santimaiden290@gmail.com>
2017-11-23 16:21:34 -0300

    Reduce frequency of checks

--- app.js
+++ app.js
@@ -27,47 +27,52 @@ const parameters = [
     "misteriuous"
 ]
 
-let relevantResults = [];
+//store all posts that match parameters here to loop through later
+let relevantPosts = [];
+//keep a record of who got sent a message so that nobody gets spammed by accident
+// key: user name value: relevant url
 let recordOfUsers = {};
 
 
 setInterval( () =>{
-r.getSubreddit("Askreddit").getHot().then((posts) => {
+    console.log("Scanning...");
+    r.getSubreddit("Askreddit").getHot().then((posts) => {
 
-    posts.forEach(element => {
-        for(let i = 0; i < parameters.length; i++){
-            if(element.title.toLowerCase().indexOf(parameters[i]) !== -1){
-                console.log(element.title);
-                if(relevantResults.indexOf(element.url) === -1){
-                    relevantResults.push(element.url);
+        posts.forEach(element => {
+            for(let i = 0; i < parameters.length; i++){
+                if(element.title.toLowerCase().indexOf(parameters[i]) !== -1 && element.id !== '7es84h'){
+                    if(relevantPosts.indexOf(element.url) === -1){
+                        relevantPosts.push(element.url);
+                    }
                 }
             }
-        }
-    });
-
-    if(relevantResults.length > 0){
-
-        r.getSubmission('7efxig').expandReplies({limit:Infinity, depth: Infinity}).then(threadObj =>{
-            threadObj.comments.forEach(comment =>{
-                if(!recordOfUsers.hasOwnProperty(comment.author.name.toString())){
-                    recordOfUsers[comment.author.name] = [];
-                    console.log(recordOfUsers);
-                }
-
-                relevantResults.forEach(result => {
-                    if(recordOfUsers[comment.author.name].indexOf(result) === -1){
-                        r.composeMessage({
-                            to: comment.author.name,
-                            subject: 'New creepy askreddit! (CreepyAskredditBot)',
-                            text: 'New creepy thread! \n\n' + 
-                                    result.toString() + "\n\n" +  
-                                    "*if this thread has already been sent to you, [click here to know why](https://www.reddit.com/r/CreepyAskredditBot/comments/7eucv2/if_you_got_a_duplicate_message_heres_why/)*"
-                        });        
-                        recordOfUsers[comment.author.name].push(result.toString());       
-                    }
-                });
-            });
         });
 
-    }
-})}, 5000);
+        if(relevantPosts.length > 0){
+
+            r.getSubmission('7efxig').expandReplies({limit:Infinity, depth: Infinity}).then(threadObj =>{
+                threadObj.comments.forEach(comment =>{
+                    if(!recordOfUsers.hasOwnProperty(comment.author.name.toString())){
+                        recordOfUsers[comment.author.name] = [];
+                        console.log(recordOfUsers);
+                    }
+
+                    relevantPosts.forEach(post => {
+                        if(recordOfUsers[comment.author.name].indexOf(post) === -1){
+                            r.composeMessage({
+                                to: comment.author.name,
+                                subject: 'New creepy askreddit! (CreepyAskredditBot)',
+                                text: 'New creepy thread! \n\n' + 
+                                        post.toString() + "\n\n" +  
+                                        "*if this thread has already been sent to you, [click here to know why](https://www.reddit.com/r/CreepyAskredditBot/comments/7eucv2/if_you_got_a_duplicate_message_heres_why/)*"
+                            });        
+                            recordOfUsers[comment.author.name].push(post.toString()); 
+                            console.log("Sent: " + post.toString() + "to: " + comment.author.name);
+                        }
+                    });
+                });
+            });
+
+        }
+    });
+}, 1800 * 1000); // every 30 minutes
