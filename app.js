8f64b3b3f78d39e02c1f8fae985cd52b8b026569
Santi Valdez <santimaiden290@gmail.com>
2017-12-01 06:08:34 -0300

    Putting anti-spam bandaid to get the bot back up without spamming

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ setInterval( () =>{
 
         posts.forEach(element => {
             for(let i = 0; i < parameters.length; i++){
-                if(element.title.toLowerCase().indexOf(parameters[i]) !== -1){
+                if(element.title.toLowerCase().indexOf(parameters[i]) !== -1 && element.id !== "7gpns0"){
                     if(relevantPosts.indexOf(element.url) === -1){
                         relevantPosts.push(element.url);
                     }
