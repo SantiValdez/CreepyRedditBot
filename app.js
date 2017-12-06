54f29e87ca397c49ab211d564338d9f27a2a5434
Santi Valdez <santimaiden290@gmail.com>
2017-12-06 17:30:08 -0300

    Add automatic unsubscribe feature + fix bug to not send old posts to new users

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ const parameters = [
     "otherworldly"
 ]
 
+const unsubscribeParams = ["unsubscribe", "stop", "don't message", "dont message", "stop messaging"];
+
 //store all posts that match parameters here to loop through later
 let relevantPosts = [];
 //keep a record of who got sent a message so that nobody gets spammed by accident
@@ -69,18 +71,31 @@ let totalUsers = 0;
 
 
 setInterval( () =>{
+
+    let allUrls = [];
+
     r.getSubreddit("Askreddit").getHot().then((posts) => {
         console.log("Scanning...");
 
         posts.forEach(element => {
             for(let i = 0; i < parameters.length; i++){
-                if(element.title.toLowerCase().indexOf(parameters[i]) !== -1 && element.id !== "7h3l7r" && element.id !== "7h69gl" && element.id !== "7h6rur"){
+                if(element.title.toLowerCase().indexOf(parameters[i]) !== -1){
                     if(relevantPosts.indexOf(element.url) === -1){
                         relevantPosts.push(element.url);
                     }
                 }
             }
+            allUrls.push(element.url);
         });
+
+        //Remove posts that get pushed out so that they dont get sent to new users.
+        for (let i = 0; i < relevantPosts.length; i++) {
+            if(allUrls.indexOf(relevantPosts[i]) === -1){
+                console.log("Removing - " + relevantPosts[i] + " - as it's not in the front-page anymore.");
+                relevantPosts.splice(relevantPosts[i], 1);
+            }
+        }
+
 
         if(relevantPosts.length > 0){
 
@@ -98,7 +113,8 @@ setInterval( () =>{
                                 subject: 'New creepy askreddit! (CreepyAskredditBot)',
                                 text: '**New creepy thread!** \n\n' + 
                                         post.toString() + "\n\n" +  
-                                        "*if this thread has already been sent to you, [click here to know why](https://www.reddit.com/r/CreepyAskredditBot/comments/7eucv2/if_you_got_a_duplicate_message_heres_why/)*"
+                                        '*if this thread has already been sent to you, [click here to know why](https://www.reddit.com/r/CreepyAskredditBot/comments/7eucv2/if_you_got_a_duplicate_message_heres_why/)* \n\n' +
+                                        '**[^click ^here ^to ^stop ^receiving ^messages](https://www.reddit.com/r/CreepyAskredditBot/comments/7i0uh4/want_to_unsubscribe_read_here/)**'
                             });        
                             recordOfUsers[comment.author.name].push(post.toString()); 
                             console.log("Sent: " + post.toString() + "to: " + comment.author.name);
@@ -109,5 +125,32 @@ setInterval( () =>{
             });
 
         }
+
+        //get all unread PM's
+        r.getInbox('unread').then(messages => {
+            messages.forEach(message => {
+                if(message.new){
+                    unsubscribeParams.forEach(param => {
+                        //if a message contains unsubscribe keyword look that user up in the thread and delete their comment
+                        if(message.body.indexOf(param) !== -1){
+                            r.getSubmission('7efxig').expandReplies({limit:Infinity, depth: Infinity}).then(threadObj =>{
+                                threadObj.comments.forEach(comment =>{
+                                    if(comment.author.name === message.author.name){
+                                        comment.delete();
+
+                                        r.composeMessage({
+                                            to: comment.author.name,
+                                            subject: 'You have been unsubscribed. (CreepyAskredditBot)',
+                                            text: "You have been unsubscribed from CreepyAskredditBot. You will not receive messaged anymore. \n\n"
+                                            + "If you wish to subscribe again, just leave a comment on [this thread](https://www.reddit.com/r/CreepyAskredditBot/comments/7efxig/comment_here_to_get_notified_by_the_bot/)"
+                                        }); 
+                                    }
+                                });
+                            });
+                        }
+                    });
+                }
+            });
+        });
     });
 }, 15 * 60 * 1000); // every 15 minutes
