e65fae706f4203237d952fa6ae696b4c20882738
Santi Valdez <santimaiden290@gmail.com>
2017-12-08 18:22:17 -0300

    And back again

--- app.js
+++ app.js
@@ -148,6 +148,7 @@ setInterval( () =>{
                                     }
                                 });
                             });
+                            message.markAsRead();
                         }
                     });
                 }//
